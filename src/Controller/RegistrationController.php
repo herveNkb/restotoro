543fd9bf5d39ba37a764a740a92dae3e84d2639c
AirV <herve.jacotet@sfr.fr>
2023-02-17 17:37:57 +0100

    feat(verify_email): Verify email Add entity and redirect to route for verify email.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -82,8 +82,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse email a bien été vérifiée.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_main');
     }
 }
