74e378f87e034d7d648bed986b442667e2a4e37f
AirV <herve.jacotet@sfr.fr>
2023-02-28 10:58:58 +0100

    deploy(dev-bundle): removing dev-bundle Remove dev bundle for deploy in line.

--- config/bundles.php
+++ config/bundles.php
@@ -11,7 +11,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
