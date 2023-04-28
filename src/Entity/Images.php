0876064dc757dda265b49f77208d7446557b5922
AirV <herve.jacotet@sfr.fr>
2023-04-28 17:51:55 +0200

    feat($imageFile): add property add $imageFile property with her getter and her setter.

--- src/Entity/Images.php
+++ src/Entity/Images.php
@@ -4,8 +4,11 @@ namespace App\Entity;
 
 use App\Repository\ImagesRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ImagesRepository::class)]
+#[Vich\Uploadable]
 class Images
 {
     #[ORM\Id]
@@ -18,6 +21,10 @@ class Images
 
     #[ORM\Column(length: 255)]
     private ?string $image_name = null;
+
+    #[Vich\UploadableField(mapping: 'featured_image', fileNameProperty: 'image_name')]
+    // featured_images is the name of the mapping in config/packages/vich_uploader.yaml
+    private ?File $imageFile = null;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $updated_at = null;
@@ -53,6 +60,22 @@ class Images
         $this->image_name = $image_name;
 
         return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this -> imageFile = $imageFile;
+
+        if (null !== $imageFile) { // if 'imageFile' has been set
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this -> updated_at = new \DateTimeImmutable(); // set 'updatedAt'
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this -> imageFile;
     }
 
     public function getUpdatedAt(): ?\DateTimeImmutable
