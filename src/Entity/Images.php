115c81f43bbe441239828f574f4f918bdd44b6a1
AirV <herve.jacotet@sfr.fr>
2023-04-28 16:19:51 +0200

    feat(images entity): new entity Creation of entity class for images main-page.

---
+++ src/Entity/Images.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImagesRepository::class)]
+class Images
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image_title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image_name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updated_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImageTitle(): ?string
+    {
+        return $this->image_title;
+    }
+
+    public function setImageTitle(string $image_title): self
+    {
+        $this->image_title = $image_title;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->image_name;
+    }
+
+    public function setImageName(string $image_name): self
+    {
+        $this->image_name = $image_name;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
