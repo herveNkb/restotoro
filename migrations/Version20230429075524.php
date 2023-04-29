62a7e501eb3460131b9e6e89b9d3a9ea9cfc9f4b
AirV <herve.jacotet@sfr.fr>
2023-04-29 14:37:48 +0200

    fix(entity): property image_name Fixed problem to property of string in image_name.

---
+++ migrations/Version20230429075524.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230429075524 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE images CHANGE image_title image_title VARCHAR(255) DEFAULT NULL, CHANGE image_name image_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE images CHANGE image_title image_title VARCHAR(255) NOT NULL, CHANGE image_name image_name VARCHAR(255) NOT NULL');
+    }
+}
