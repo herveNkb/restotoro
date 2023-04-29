769a3e55d7675a50665fa2176d5324c568d1f4f2
AirV <herve.jacotet@sfr.fr>
2023-04-29 20:56:56 +0200

    feat(image title): dynamic diplay Add script in JS for dynamic display or hide title on mousse hover.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+// Selects all elements with class image-home-container
+// and stores them in the imageContainers constant.
+const imageContainers = document.querySelectorAll('.image-home-container');
+
+// We iterate over each element of the constant image Containers
+// with forEach and stores them in the constant container..
+imageContainers.forEach(container => {
+    // Retrieves the element with the class image-title-container
+    // and stores it in the titleContainer constant.
+    const titleContainer = container.querySelector('.image-title-container');
+
+    // Add the first event listener on the container element.
+    container.addEventListener('mouseenter', () => {
+        titleContainer.style.display = 'block';
+    });
+
+    // Add the second event listener on the container element.
+    container.addEventListener('mouseleave', () => {
+        titleContainer.style.display = 'none';
+    });
+});
