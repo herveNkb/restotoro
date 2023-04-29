c39408cbdeb7828c276984510318ae45313fc63b
AirV <herve.jacotet@sfr.fr>
2023-04-29 18:57:44 +0200

    feat(main page): retrieve data Add ManagerRegistry for retrieve data from database.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Images;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // Retrieve data from database
+        // Replaces getDoctrine() which is deprecated
+        $images = $doctrine -> getRepository(Images::class) -> findAll();
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'images' => $images
         ]);
     }
 }
