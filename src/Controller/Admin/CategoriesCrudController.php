ef860e67e96c501c0edf099c22aa846bde771509
AirV <herve.jacotet@sfr.fr>
2023-05-01 10:36:19 +0200

    feat(back-office): crud categories Create CRUD controller for categories into back-office.

---
+++ src/Controller/Admin/CategoriesCrudController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Categories;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CategoriesCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Categories::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            yield TextField ::new('dish_categorie', 'Nom de la catégorie'),
+        ];
+    }
+}
