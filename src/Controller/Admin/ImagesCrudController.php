5e0684a72d638e23ab2630b44d375e592391297d
AirV <herve.jacotet@sfr.fr>
2023-05-01 15:19:50 +0200

    fix(easyAdmin): indent code Indented code.

--- src/Controller/Admin/ImagesCrudController.php
+++ src/Controller/Admin/ImagesCrudController.php
@@ -16,15 +16,13 @@ class ImagesCrudController extends AbstractCrudController
         return Images::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
         return [
             yield TextField ::new('image_title', 'Titre de l\'image'),
             yield DateField ::new('updatedAt', 'Date de création') -> hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
             yield TextField ::new('imageFile', 'Taille maximum image : 2MB') -> setFormType(VichImageType::class) -> onlyOnForms(), // onlyOnForms() is used to display the field only on the new and edit pages
-            yield ImageField ::new('image_name', 'Apercu') -> setBasePath('uploads/images') -> onlyOnIndex() // onlyOnIndex() is used to display the field only on the index page
+            yield ImageField ::new('image_name', 'Apercu') -> setBasePath('uploads/images')
         ];
     }
-
 }
