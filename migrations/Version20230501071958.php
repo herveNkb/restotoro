b61c7e6431b063e67295cc2b7afa2c7ac9e4efc9
AirV <herve.jacotet@sfr.fr>
2023-05-01 09:26:53 +0200

    feat(migration): new entity Create new Categories entity.

---
+++ migrations/Version20230501071958.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230501071958 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categories CHANGE dish_category dish_categorie VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE menus DROP categories_id');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categories CHANGE dish_categorie dish_category VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE menus ADD categories_id INT NOT NULL');
+    }
+}
