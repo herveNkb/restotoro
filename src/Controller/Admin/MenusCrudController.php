e3743cdb8e899791ff5aea346c2e4cc68ec2df88
AirV <herve.jacotet@sfr.fr>
2023-05-01 07:12:32 +0200

    feat(back-office): crud menus Add field for CRUD menus into easyAdmin.

---
+++ src/Controller/Admin/MenusCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Menus;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class MenusCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Menus::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            yield TextField ::new('dish_title', 'Nom du plat'),
+            yield IntegerField ::new('dish_price', 'Prix'),
+            yield TextField ::new('dish_description', 'Description'),
+        ];
+    }
+
+}
