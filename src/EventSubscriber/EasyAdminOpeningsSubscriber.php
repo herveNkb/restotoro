10de1f381117b9e0b54ace6ad58e2cc9131cf292
AirV <herve.jacotet@sfr.fr>
2023-05-02 20:44:43 +0200

    feat(eventSubscriber): security method Add eventSubscriber for get users id and put it in foreign key into openings entity.

---
+++ src/EventSubscriber/EasyAdminOpeningsSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Openings;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Security;
+
+
+// Allows to retrieve the connected user to associate it with the Formulas (user_id)
+class EasyAdminOpeningsSubscriber implements EventSubscriberInterface
+{
+// Security is a service that allows to get the current user
+    private Security $security;
+
+// We inject the Security service in the constructor
+    public function __construct(Security $security)
+    {
+        $this -> security = $security;
+    }
+
+// This method is called a event when an entity is persisted
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['setOpeningsDayMorningAndAfternoon'],
+        ];
+    }
+
+// We retrieve the instance of the Formulas entity
+    public function setOpeningsDayMorningAndAfternoon(BeforeEntityPersistedEvent $event): void
+    {
+        $entity = $event -> getEntityInstance();
+// Check that the entity is indeed an instance of Formulas
+        if (!($entity instanceof Openings)) {
+            return;
+        }
+// Retrieve the current user
+        $user = $this -> security -> getUser();
+// Send the user to the entity
+        $entity -> setUsers($user);
+    }
+}
