f12bb87f7c6902c9138b91d481c81b4b4ae49632
AirV <herve.jacotet@sfr.fr>
2023-05-02 16:01:04 +0200

    feat(back-office): formulas interface Create FormulaCrudController for created formulas of dishes.

---
+++ src/Controller/Admin/FormulasCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Formulas;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class FormulasCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Formulas::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            yield TextField ::new('formula_title', 'Nom de la formule'),
+            yield IntegerField ::new('formula_price', 'Tarif'),
+            yield TextEditorField::new('description', 'Description')->hideOnIndex()->setNumOfRows(20), // hideOnIndex() is used to hide the field on the index page,
+        ];
+    }
+
+}
