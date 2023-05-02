c53675a01a8a32148bdaf95cc7b5feee8f163893
AirV <herve.jacotet@sfr.fr>
2023-05-02 16:12:55 +0200

    feat(page "nos-menus"): controller Create Formulas Controller and add ManagerRegistry.

---
+++ src/Controller/FormulasController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Formulas;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormulasController extends AbstractController
+{
+    #[Route('/nos-menus', name: 'app_formulas')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        // Retrieve data from database
+        // Replaces getDoctrine() which is deprecated
+        $formulas = $doctrine -> getRepository(Formulas::class) -> findAll();
+        return $this->render('formulas/index.html.twig', [
+            'formulas' => $formulas
+        ]);
+    }
+}
