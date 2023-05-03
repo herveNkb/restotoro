15818cb8d3c7ca90124515eff3f7096fed90843b
AirV <herve.jacotet@sfr.fr>
2023-05-03 19:35:32 +0200

    feat(schedule): schedule controller Create schedule feature for include into some page with a template.

--- src/Controller/MenusController.php
+++ src/Controller/MenusController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Menus;
+use App\Entity\Openings;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,6 +18,13 @@ class MenusController extends AbstractController
         // Replaces getDoctrine() which is deprecated
         $menus = $doctrine -> getRepository(Menus::class) -> findAll();
 
+        // Retrieve the opening hours from the database
+        $openings = $doctrine -> getRepository(Openings::class) -> findAll();
+        // Render the view of the opening hours table with "renderView" method
+        $openingsView = $this -> renderView('_partials/_openings.html.twig', [
+            'openings' => $openings
+        ]);
+
         //  Tableau qui sert à grouper les menus par catégorie
         $groupedMenus = [];
         // Boucle qui itére sur les menus récupérés précédemment avec la méthode findAll()
@@ -27,7 +35,8 @@ class MenusController extends AbstractController
             $groupedMenus[$categoryName][] = $menu;
         }
         return $this->render('menus/index.html.twig', [
-            'groupedMenus' => $groupedMenus
+            'groupedMenus' => $groupedMenus,
+            'openingsView' => $openingsView
         ]);
     }
 }
