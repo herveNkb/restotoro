f5c378edc207006258f80420519013e48d2dd105
AirV <herve.jacotet@sfr.fr>
2023-05-08 20:02:37 +0200

    feat(dashboard): create sub-menu Create subMenu for ReservationsSettings interface.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Entity\Formulas;
 use App\Entity\Images;
 use App\Entity\Menus;
 use App\Entity\Openings;
+use App\Entity\ReservationsSettings;
 use App\Entity\Users;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
@@ -22,27 +23,30 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        return $this -> render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
-        return Dashboard::new()
-            ->setTitle('Le Quai Antique');
+        return Dashboard ::new()
+            -> setTitle('Le Quai Antique');
     }
 
 // Right menu
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-utensils');
-         yield MenuItem::linkToCrud('Images page d\'accueil', 'fas fa-image', Images::class);
-        yield MenuItem::subMenu('gestion de la carte', 'fa fa-utensils')->setSubItems([
-          MenuItem::linkToCrud('Carte des plats', 'fas fa-utensils', Menus::class),
-          MenuItem::linkToCrud('Catégories des plats', 'fas fa-list', Categories::class)
+        yield MenuItem ::linkToDashboard('Tableau de bord', 'fa fa-utensils');
+        yield MenuItem ::linkToCrud('Images page d\'accueil', 'fas fa-image', Images::class);
+        yield MenuItem ::subMenu('gestion de la carte', 'fa fa-utensils') -> setSubItems([
+            MenuItem ::linkToCrud('Carte des plats', 'fas fa-utensils', Menus::class),
+            MenuItem ::linkToCrud('Catégories des plats', 'fas fa-list', Categories::class)
         ]);
-         yield MenuItem::linkToCrud('Formules de la carte', 'fas fa-clipboard', Formulas::class);
-         yield MenuItem::linkToCrud('Horaires d\'ouverture', 'fas fa-clock', Openings::class);
-         yield MenuItem::linkToCrud('Profils', 'fas fa-user', Users::class);
+        yield MenuItem ::linkToCrud('Formules de la carte', 'fas fa-clipboard', Formulas::class);
+        yield MenuItem ::linkToCrud('Horaires d\'ouverture', 'fas fa-clock', Openings::class);
+        yield MenuItem ::subMenu('Gestion de la réservation', 'fas fa-users') -> setSubItems([
+            MenuItem ::linkToCrud('Heures de service', 'fas fa-clock', ReservationsSettings::class),
+        ]);
+        yield MenuItem ::linkToCrud('Profils', 'fas fa-user', Users::class);
         yield MenuItem ::linkToUrl('Retour à l\'accueil', 'fas fa-home', $this -> generateUrl('app_main'));
     }
 
