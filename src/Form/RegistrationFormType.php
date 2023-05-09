e7c50ff3f405543b0ad811a8ce5b81fe69237484
AirV <herve.jacotet@sfr.fr>
2023-05-09 06:53:52 +0200

    feat(registration form): add new fields Add new fields into from registration with constraints.

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,11 +5,16 @@ namespace App\Form;
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
@@ -17,7 +22,13 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'constraints' => [
+                    new Regex('/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/',
+                        "L'adresse email n'est pas valide")
+                ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'label' => 'Vous acceptez que votre email soit utilisé pour pouvoir vous connecter',
@@ -57,6 +68,29 @@ class RegistrationFormType extends AbstractType
                         "Le nom ne doit contenir que des lettres, sans accents")
                 ],
             ])
+            ->add('defaultCustomerNumber', IntegerType::class, [
+                'label' =>'Nombre de convives',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^(?!.*\d{3,})\d+$/',
+                        'message' => 'Le nombre de convives doit être une valeur numérique avec un maximum de deux chiffres consécutifs.',
+                    ]),
+                    new GreaterThanOrEqual([
+                        'value' => 1,
+                        'message' => 'Le nombre de convives doit être au moins de {{ compared_value }}.',
+                    ]),
+                    new LessThanOrEqual([
+                        'value' => 20,
+                        'message' => 'Le nombre de convives ne peut pas dépasser {{ compared_value }}.',
+                    ]),
+                ],
+            ])
+            -> add('default_allergies', TextareaType::class, [
+                'label' => 'Allergies - (Séparez les allergies par un trait d\'union)',
+                'attr' => [
+                    'class' => 'auto-resize'
+                ],
+        ])
         ;
     }
 
