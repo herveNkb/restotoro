6680180e1b807db1f4f982c517b7ce27329eaf5a
AirV <herve.jacotet@sfr.fr>
2023-05-11 22:34:51 +0200

    feat(back-office): security upload Add method for MimeType, size, and weight limit when upload images.

--- src/Controller/Admin/ImagesCrudController.php
+++ src/Controller/Admin/ImagesCrudController.php
@@ -7,6 +7,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ImagesCrudController extends AbstractCrudController
@@ -20,9 +23,59 @@ class ImagesCrudController extends AbstractCrudController
     {
         return [
             yield TextField ::new('image_title', 'Titre de l\'image'),
-            yield DateField ::new('updatedAt', 'Date de création') -> hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
-            yield TextField ::new('imageFile', 'Taille maximum image : 2MB') -> setFormType(VichImageType::class) -> onlyOnForms(), // onlyOnForms() is used to display the field only on the new and edit pages
-            yield ImageField ::new('image_name', 'Apercu') -> setBasePath('uploads/images') -> setUploadDir('public/uploads/images') -> onlyOnIndex(), // onlyOnIndex() is used to display the field only on the index page
+            yield DateField ::new('updatedAt', 'Date de création')
+                -> hideOnForm(), // hideOnForm() is used to hide the field on the new and edit pages
+            yield TextField ::new('imageFile', 'Poids max: 200 Ko, Taille max: 500x500 px, Formats autorisés: jpeg, jpg, png, gif')
+                -> setFormType(VichImageType::class)
+                -> onlyOnForms() // onlyOnForms() is used to display the field only on the new and edit pages
+                ->setFormTypeOptions([ // this option allows you to add a file input field with the accept="image/*" attribute
+                    'constraints' => [
+                        new Callback([$this, 'validateImageMimeTypeSizeWidthHeight'])
+                    ]
+                ]),
+            yield ImageField ::new('image_name', 'Apercu')
+                -> setBasePath('uploads/images')
+                -> setUploadDir('public/uploads/images')
+                -> onlyOnIndex(), // onlyOnIndex() is used to display the field only on the index page
         ];
+    }
+
+
+    // This method is used to validate the MIME type, file size and image dimensions.
+    public function validateImageMimeTypeSizeWidthHeight(UploadedFile $file, ExecutionContextInterface $context): void
+    {
+        // Define the allowed MIME types, maximum file size and image dimensions
+        $allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+        $maxSize = 200 * 1024; // 200 Ko
+        $maxWidth = 500;
+        $maxHeight = 500;
+
+        // Validate MIME type
+        $fileMimeType = $file->getMimeType(); // getMimeType() is a method of the UploadedFile class
+        // If the MIME type is not in the array of allowed types, add a violation
+        if (!in_array($fileMimeType, $allowedMimeTypes, true)) {
+            $context->buildViolation('Format d\'image invalide. Types de formats autorisés : {{ types }}.')
+                ->setParameter('{{ types }}', implode(', ', $allowedMimeTypes)) // {{ types }} is replaced by the list of allowed MIME types
+                ->addViolation(); // This method adds a violation with the message above
+        }
+
+        // Validate file size
+        $fileSize = $file->getSize(); // getSize() is a method of the UploadedFile class
+        // If the file size is greater than the maximum allowed size, add a violation
+        if ($fileSize > $maxSize) {
+            $context->buildViolation('L\'image est trop lourde. Poids maximum: 200 Ko.')
+                ->setParameter('{{ size }}', $maxSize)
+                ->addViolation();
+        }
+
+        // Validate image dimensions
+        [$width, $height] = getimagesize($file->getPathname()); // getimagesize() is a PHP function that returns an array containing the width and height of the image
+        // If the image dimensions are greater than the maximum allowed dimensions, add a violation
+        if ($width > $maxWidth || $height > $maxHeight) {
+            $context->buildViolation('Les dimensions de l\'image sont trop grandes. Dimensions maximales : {{ width }}x{{ height }} px.')
+                ->setParameter('{{ width }}', $maxWidth)
+                ->setParameter('{{ height }}', $maxHeight)
+                ->addViolation();
+        }
     }
 }
