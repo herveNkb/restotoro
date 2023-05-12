b55a3bcacd5116e3fb258c25af38a1f9a0ac1383
AirV <herve.jacotet@sfr.fr>
2023-05-12 04:56:59 +0200

    chore(dev bundle): temporally remove Temporally remove dev bundle for push to o2switch.

--- config/bundles.php
+++ config/bundles.php
@@ -13,7 +13,4 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
